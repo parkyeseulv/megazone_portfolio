7ab55d73d7a50ff1c2e000c40abde9884445cf73
Park Yeseul <dntwkzz79@gmail.com>
2022-01-03 10:20:59 +0900

    Delete GCP_personal_project directory

--- GCP_personal_project/bts_review_app_source_code/run_worker.sh
+++
@@ -1,8 +0,0 @@
-#echo "Exporting GCLOUD_PROJECT"
-#export GCLOUD_PROJECT='hybrid-bts'
-
-#echo "Switching to virtual environment"
-#source ~/venvs/reviewapp/bin/activate
-
-#echo "Starting worker"
-#python -m quiz.console.worker
